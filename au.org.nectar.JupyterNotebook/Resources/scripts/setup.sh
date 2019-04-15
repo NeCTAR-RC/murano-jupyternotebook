1d0a0218b7bea66807eb077ac4e767bbcefc04df
Andy Botting <andy@andybotting.com>
2019-04-15 17:32:11 +1000

    Update with latest changes

--- au.org.nectar.JupyterNotebook/Resources/scripts/setup.sh
+++ au.org.nectar.JupyterNotebook/Resources/scripts/setup.sh
@@ -59,8 +59,17 @@ EOF
 
   # Rename the user
   usermod --login $USERNAME $ORIGUSER
-  # Set and move home dir to new location, and make 'users' the primary group
-  usermod --home $MOUNT/$USERNAME --move-home --gid users --comment $USERNAME $USERNAME
+  if [ -d $MOUNT/$USERNAME ]; then
+    # Set home dir to existing volume, and make 'users' the primary group
+    usermod --home $MOUNT/$USERNAME --gid users --comment $USERNAME $USERNAME
+    # Append cloud-init ssh key to authorized_keys file in existing home
+    cat /home/$ORIGUSER/.ssh/authorized_keys >> $MOUNT/$USERNAME/.ssh/authorized_keys
+    # cloud-init home dir not needed so remove it
+    rm -rf /home/$ORIGUSER
+  else
+    # Set and move home dir to new location, and make 'users' the primary group
+    usermod --home $MOUNT/$USERNAME --move-home --gid users --comment $USERNAME $USERNAME
+  fi
 
   # Replace cloud user in cloud-init
   sed -i -e "s/name: $ORIGUSER/name: $USERNAME/g" -e "s/gecos: .*/gecos: $USERNAME/g" /etc/cloud/cloud.cfg
